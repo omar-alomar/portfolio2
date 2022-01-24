04e154b5de414515294a9d42d0bb0d07eec8baff
Omar Alomar <omaro2000@gmail.com>
2022-01-24 00:09:11 -0500

    Basic fade in home animations

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 export default function Home() {
   return (
       <>
-      Home
+      <div className="home">
+        <div className="home__greeting">
+          <h1 className="home__greeting--header">Hi,</h1>
+          <h2 className="home__greeting--header">I'm Omar.</h2>
+          <h3 className="home__greeting--header">Web Developer.</h3>
+          <p className="home__greeting--p">Check out my stuff.</p>
+        </div>
+      </div>
       </>
   );
 }
