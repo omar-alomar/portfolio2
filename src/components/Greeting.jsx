ac62410299d4b29318786f0f0676954135ca6b69
Omar Alomar <omaro2000@gmail.com>
2022-01-24 17:23:36 -0500

    finished greeting component, fixed context bug

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,53 @@
+import React, {useState, useContext } from 'react';
+import Typewriter from "typewriter-effect"
+import { RenderContext } from '../App'
+
+
+
+export default function Greeting() {
+  const { rendered, setRendered } = useContext(RenderContext)
+
+  // updates rendered state after animation 
+  setTimeout(() => {
+    setRendered(true)
+  },15000);
+if (rendered === false) {
+return (
+      <>
+      <h1 className="home__greeting--header">
+        <Typewriter 
+            options={{
+                skipAddStyles: true
+            }}
+            onInit={(typewriter) => {
+            typewriter.typeString("<span>Hi</span>")
+            .pauseFor(200)
+            typewriter.typeString("<span>.</span>")
+            .pauseFor(1000)
+            .typeString("<br/>I'm Omar,")
+            .pauseFor(1000)
+            .typeString("<br/>Web designer.")
+            .pauseFor(1000)
+            .deleteChars(7)
+            .typeString("veloper.")
+            .pauseFor(1000)
+            .typeString("<br/><span style='color: #8d8d8d; font-size: 40px;'>Check out my stuff!</span>")
+            .start();
+              }}
+            />
+        </h1>
+      </>
+  );
+}
+else return (
+    <>
+        <h1 className="home__greeting--header">
+           Hi.<br/>
+           I'm Omar,<br/>
+           Web Developer. <br/>
+           <span style={{color: '#8d8d8d', fontSize: '40px'}}>Check out my stuff!</span>
+        </h1>
+
+    </>
+)
+}
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,39 +1,13 @@
-import React, { useState, useContext } from 'react';
-import Typewriter from "typewriter-effect"
+import React from 'react';
 import Socials from '../Socials';
-import { RenderContext } from '../../App'
+import Greeting from '../Greeting';
 
 export default function Home() {
-  const { rendered, setRendered } = useContext(RenderContext)
-  
   return (
       <>
       <div className="home">
         <div className="home__greeting">
-          <h1 className="home__greeting--header">
-            <Typewriter 
-              onInit={(typewriter) => {
-                typewriter.typeString("<strong><span style='font-size: 2.5rem;'>Hi</span></strong>")
-                .pauseFor(200)
-                typewriter.typeString("<strong><span style='font-size: 2.5rem;'>,</span></strong>")
-                .pauseFor(1000)
-                .typeString("<br/><strong>I'm Omar.</strong>")
-                .pauseFor(1000)
-                .typeString("<br/><strong>Web designer.</strong>")
-                .pauseFor(1000)
-                .deleteChars(7)
-                .typeString("<strong>veloper.</strong>")
-                .pauseFor(1000)
-                .typeString("<br/><span style='color: #8d8d8d; font-size: 40px;'>Check out my stuff!</span>")
-                .callFunction(() => {
-                  setRendered(true)
-                  console.log(rendered)
-                })
-                .start()
-              }}
-            />
-
-            </h1>
+            <Greeting />
          </div>
          <div className="socials-container">
           <Socials />
@@ -41,4 +15,5 @@ export default function Home() {
       </div>
       </>
   );
+  
 }
