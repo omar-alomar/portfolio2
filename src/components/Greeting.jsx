da9fe1bb2cf680a77700a5ec75753c6fe7bf4b81
Omar Alomar <omaro2000@gmail.com>
2022-01-27 10:18:03 -0500

    added home particles, navbar transition

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -3,6 +3,8 @@ import Typewriter from "typewriter-effect"
 import { RenderContext } from '../App'
 import Socials from "./Socials"
 import SocialsStatic from "./SocialsStatic"
+import Particles from "react-tsparticles";
+
 
 export default function Greeting() {
   const { rendered, setRendered } = useContext(RenderContext)
@@ -11,6 +13,16 @@ export default function Greeting() {
   setTimeout(() => {
     setRendered(true)
   },15000);
+
+  const particlesInit = (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
 if (rendered === false) {
 return (
       <>
@@ -53,6 +65,85 @@ else return (
         <div className="socials-container">
           <SocialsStatic />
         </div>
+        <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            style={{marginTop: "6vh"}}
+            height="94vh"
+            options={{
+              fpsLimit: 60,
+              interactivity: {
+                events: {
+                  onClick: {
+                    enable: true,
+                    mode: "push",
+                  },
+                  onHover: {
+                    enable: true,
+                    mode: "repulse",
+                  },
+                  resize: true,
+                },
+                modes: {
+                  bubble: {
+                    distance: 400,
+                    duration: 2,
+                    opacity: 0.8,
+                    size: 40,
+                  },
+                  push: {
+                    quantity: 4,
+                  },
+                  repulse: {
+                    distance: 100,
+                    duration: 0.4,
+                  },
+                },
+              },
+              particles: {
+                color: {
+                  value: "#08fdd8",
+                },
+                links: {
+                  color: "#08fdd8",
+                  distance: 150,
+                  enable: true,
+                  opacity: 0.5,
+                  width: 1,
+                },
+                collisions: {
+                  enable: true,
+                },
+                move: {
+                  direction: "none",
+                  enable: true,
+                  outMode: "bounce",
+                  random: false,
+                  speed: 6,
+                  straight: false,
+                },
+                number: {
+                  density: {
+                    enable: true,
+                    area: 1600,
+                  },
+                  value: 80,
+                },
+                opacity: {
+                  value: 0.5,
+                },
+                shape: {
+                  type: "circle",
+                },
+                size: {
+                  random: true,
+                  value: 5,
+                },
+              },
+              detectRetina: true,
+          }}
+        />
 
     </>
 )
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Greeting from '../Greeting';
 
 export default function Home() {
+
   return (
       <>
       <div className="home">
         <div className="home__greeting">
             <Greeting />
          </div>
+
       </div>
       </>
   );
